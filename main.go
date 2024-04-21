f742488b500ed6459756d1f30c7702e1e568e838
Dennis Gilli <dennis.gilli@acribis.ch>
2024-04-21 12:54:22 +0200

    add streaming logic

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"crypto/rand"
+	"encoding/binary"
 	"fmt"
 	"io"
 	"log"
@@ -29,14 +31,15 @@ func (fs *FileServer) start() {
 }
 
 func (fs *FileServer) readLoop(conn net.Conn) {
-    buf := make([]byte, 2048)
+    buf := new(bytes.Buffer)
     for {
-        n, err := conn.Read(buf)
+        var size int64
+        binary.Read(conn, binary.LittleEndian, &size)
+        n, err := io.CopyN(buf, conn, size)
         if err != nil {
             log.Fatal(err)
         }
-        file := buf[:n]
-        fmt.Println(file)
+        fmt.Println(buf.Bytes())
         fmt.Printf("received %d bytes over the network\n", n)
     }
 }
@@ -53,7 +56,8 @@ func sendFile(size int) error {
         return err
     }
 
-    n, err := conn.Write(file)
+    binary.Write(conn, binary.LittleEndian, int64(size))
+    n, err := io.CopyN(conn, bytes.NewReader(file), int64(size))
     if err != nil {
         return err
     }
@@ -63,8 +67,8 @@ func sendFile(size int) error {
 
 func main() {
     go func() {
-        time.Sleep(4 * time.Second)
-        sendFile(4000)
+        time.Sleep(2 * time.Second)
+        sendFile(130560) // fix: Why above this # file goes brrrrr?
     }()
 
     server := &FileServer{}
