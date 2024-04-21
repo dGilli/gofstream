a393ddc12f39ae8c8cb4386e0a086b5aee2598bf
Dennis Gilli <dennis.gilli@acribis.ch>
2024-04-21 11:41:14 +0200

    add file server logic and read loop

--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net"
 )
 
+type (
+    FileServer struct {}
+)
+
+func (fs *FileServer) start() {
+    ln, err := net.Listen("tcp", ":8000")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    for {
+        conn, err := ln.Accept()
+        if err != nil {
+            log.Fatal(err)
+        }
+        go fs.readLoop(conn)
+    }
+}
+
+func (fs *FileServer) readLoop(conn net.Conn) {
+    buf := make([]byte, 2048)
+    for {
+        n, err := conn.Read(buf)
+        if err != nil {
+            log.Fatal(err)
+        }
+        file := buf[:n]
+        fmt.Println(file)
+        fmt.Printf("received %d bytes over the network\n", n)
+    }
+}
+
 func main() {
+    server := &FileServer{}
+    server.start()
 }
